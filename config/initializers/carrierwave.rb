a0438a8a6d7c836cde46f800fe5736dd9689d56c
Laura Ojala <laojala@gmail.com>
2016-03-12 00:38:49 +0200

    Fixing AWS config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,9 @@
 CarrierWave.configure do |config|
-
   config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV["aws_access_key_id"],
-    :aws_secret_access_key  => ENV["aws_secret_access_key"],
-    :region => 'eu-central-1'
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['aws_access_key_id'],                        # required
+    :aws_secret_access_key  => ENV['aws_secret_access_key'],
+    :region  => 'eu-central-1'                      # required
   }
-
-  config.fog_directory  =  ENV["config.fog_directory"]
+  config.fog_directory  = ENV['fog_directory']                     # required
 end
